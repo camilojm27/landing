2c2f5adf4c38b8eddfd986002952d414ad8fb8a5
Camilo José Mezú Mina <camilo272001@gmail.com>
2021-02-26 22:09:42 -0500

    🍔Animation Burger

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
+const burgerDesk = document.querySelector("#burgerDesk");
+
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
@@ -12,4 +14,9 @@ links.forEach((link) => {
     nav.classList.toggle("nav-open");
     burger.classList.toggle("toggle");
   });
+});
+
+burgerDesk.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
 });
